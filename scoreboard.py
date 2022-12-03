870edffa8d3278c9eafae44fe9159a2bf181a6e2
panpablitto <68351154+panpablitto@users.noreply.github.com>
2022-12-03 21:43:29 +0100

    code
    snake game with all needed classes

---
+++ scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+FONT = ('Arial', 24, 'normal')
+ALIGN = "center"
+
+
+class Scoreboard(Turtle):
+     def __init__(self):
+         super().__init__()
+         self.score = 0
+         self.color("white")
+         self.penup()
+         self.goto(0,270)
+         self.hideturtle()
+         self.update_scoreboard()
+
+     def update_scoreboard(self):
+         self.write(f"Score: {self.score} ", align=ALIGN, font=FONT)
+
+     def game_over(self):
+         self.color("white")
+         self.penup()
+         self.goto(0,0)
+         self.hideturtle()
+         self.write("Game Over", align=ALIGN, font=FONT)
+
+     def incrise_score(self):
+         self.score +=1
+         self.clear()
+         self.update_scoreboard()
