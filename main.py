6e4c72c186c536636a4c0e42eb503bb656f8d444
panpablitto <68351154+panpablitto@users.noreply.github.com>
2022-12-06 19:03:33 +0100

    changes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-scoreboard =Scoreboard()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -26,18 +26,27 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    if snake.head.distance(food) <15:
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.incrise_score()
+        scoreboard.increase_score()
 
-    if snake.head.xcor() >290 or snake.head.xcor()< -290 or snake.head.ycor() >290 or snake.head.ycor()< -290:
-        game_is_on = False
-        scoreboard.game_over()
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
 
-    for segment in snake.segments[1:len(snake.segments)+1]:
-        if snake.head.distance(segment) < 5:
-            game_is_on = False
-            scoreboard.game_over()
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+
+
+
 
 screen.exitonclick()
